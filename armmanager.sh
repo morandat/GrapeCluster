6274e1860a6268f48caabc03f9ca3c0789727965
pravez <paul.ravezb@gmail.com>
2017-01-24 09:36:31 +0100

    Continued work

--- armmanager.sh
+++ armmanager.sh
@@ -2,13 +2,14 @@
 
 #Usage function to print how to use
 usage() {
-	echo "armmanager [-m] [-u] <file> [-o <destination>]"
+	echo "armmanager [-m] [-u <destination>] <file> [-o <destination>]"
 }
 
 #Global Variables
 FILE=""
 DESTINATION=""
 MOUNTOPT=0
+DIRECTORY=""
 
 #If no parameter given, wtf ?
 if [ -z "$1" ]; then
@@ -33,6 +34,8 @@ while [[ $# -gt 1 ]]; do
 		#unmount
 		-u | --unmount)
 			MOUNTOPT=2
+			DIRECTORY="$2"
+			shift
 			;;
 		#where to output
 		-o | --output)
@@ -42,6 +45,7 @@ while [[ $# -gt 1 ]]; do
 		#need to know usage
 		-h | --help)
 			usage
+			exit 0
 			;;
 		#default : input file
 		*)
@@ -51,10 +55,42 @@ while [[ $# -gt 1 ]]; do
 	shift # past argument or value
 done
 
+
 #If no file is given
-if [ FILE == "" ]
+if [ "$FILE" == "" ]
 then    
     usage 
     exit 1
 fi
 
+#Mount the filesystem
+if [ $MOUNTOPT -le 1 ]
+then
+	if [[ $FILE == *.img ]] || [[ $FILE == *.iso ]]
+	then
+		#If no destination given
+		if [ "$DESTINATION" == "" ]
+		then
+			DESTINATION="./.armanager"
+		fi
+		#Create directory for destination
+		mkdir -p $DESTINATION
+		guestmount -a $FILE -m /dev/sda2 $DESTINATION
+		if [ $? -ne 0 ]
+		then
+			echo "Something went wrong when trying to mount"
+			exit 1
+		else
+			echo "Successfully mounted $FILE to $DESTINATION"
+		fi
+	else
+		echo "Please give a valid file ... (img/iso)"
+		usage
+		exit 1
+	fi
+fi
+
+if [ $MOUNTOPT -eq 2 ]
+then
+	guestunmount $DESTINATION
+fi
