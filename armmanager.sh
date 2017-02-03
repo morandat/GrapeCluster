c1d50bb3264a1785834bf61aa21121ea3fe22893
pravez <paul.ravezb@gmail.com>
2017-02-03 21:44:07 +0100

    Ajout des options pour le chroot, on peut soit ne lancer qu'un terminal, soit ne faire qu'installer, soit aucun des deux ...

--- armmanager.sh
+++ armmanager.sh
@@ -27,6 +27,9 @@ MOUNTOPT=0
 WORKING_PATH=""
 MOUNT_ONLY=false
 CHROOT=false
+CHROOTOPTIONS=""
+POSSIBLEOPTIONS=("chroot-only" "install-only")
+FINALOPTIONS=""
 
 mount_image(){
 	echo "======> Mounting the image"
@@ -34,7 +37,7 @@ mount_image(){
 	then
 		#If no destination given
 		if [ "$DESTINATION" == "" ]
-		then
+		then $FINALOPTIONS
 			DESTINATION="./.armmanager"
 			filename=`basename $FILE`
 			WORKING_PATH="$DESTINATION/${filename%.*}"
@@ -85,7 +88,7 @@ copy_qemu(){
 		echo "Adding qemu-arm-static to /usr/bin of the image ..."
 		cd $WORKING_PATH
 		rootasked "cp (in mounted filesystem)"
-		sudo cp --remove-destination /usr/bin/qemu-arm-static ./usr/bin/
+		sudo cp --remove-destination /usr/bin/qemu-arm-static $WORKING_PATH/usr/bin/
 	else
 		echo "Couldn't find /usr/bin/qemu-arm-static which is required to translate from ARM to `uname -m`"
 		echo "Please install qemu, qemu-user and qemu-user-static (maybe search for qemu-extra depending on your linux distribution)"
@@ -119,52 +122,89 @@ unmount_image(){
 }
 
 chroot_image(){
-	echo "======> Preparing and executing change of root"
-	if [ -e "$WORKING_PATH/usr/bin/qemu-arm-static" ]
+	echo "Verifying given path ..."
+	df -h | grep $WORKING_PATH &> /dev/null
+	if [ $? -eq 0 ]
 	then
-		if command_exists update-binfmts;
+		echo "======> Preparing and executing change of root"
+		if [ -e "$WORKING_PATH/usr/bin/qemu-arm-static" ]
 		then
-			echo "Enabling Qemu ARM translation ..."
-			rootasked "binfmts"
-			sudo update-binfmts --enable qemu-arm
-
-			if [ $? -ne 0 ]
+			if command_exists update-binfmts;
 			then
-				echo "There was a problem when trying to enable qemu ARM translation to your system."
-				echo "Please refer to binfmts and try to correct this issue"
-				exit 1
+				echo "Enabling Qemu ARM translation ..."
+				rootasked "binfmts"
+				sudo update-binfmts --enable qemu-arm
+
+				if [ $? -ne 0 ]
+				then
+					echo "There was a problem when trying to enable qemu ARM translation to your system."
+					echo "Please refer to binfmts and try to correct this issue"
+					exit 1
+				else
+					echo "Successfully enabled translation through Qemu"
+					#Not activated yet for debug, really interesting ?
+					#if [ ! -e $WORKING_PATH/tmp/rasparchitect.sh ]
+					#then
+						echo "Preparing files for chroot ..."
+						mkdir -p $WORKING_PATH/tmp/armmanager
+						sudo cp --remove-destination rasparchitect.sh $WORKING_PATH/tmp/armmanager
+						#Providing daemon
+						sudo cp --remove-destination daemon.tgz $WORKING_PATH/tmp/armmanager
+						if [ $? -ne 0 ]
+						then
+							echo "Couldn't copy script for manipulation inside the image."
+							echo "Please retry or copy it manually"
+							exit 1
+						fi
+					#fi
+					echo "Going to chroot into mounted raspberry pi filesystem ..."
+					echo "Careful ! Now only works for Raspbian"
+					rootasked "chroot"
+					sudo chroot $WORKING_PATH /usr/bin/qemu-arm-static /bin/bash /tmp/armmanager/rasparchitect.sh -f=daemon.tgz $FINALOPTIONS
+					echo "Finished chroot actions"
+				fi
 			else
-				echo "Successfully enabled translation through Qemu"
-				#Not activated yet for debug, really interesting ?
-				#if [ ! -e $WORKING_PATH/tmp/rasparchitect.sh ]
-				#then
-					echo "Preparing files for chroot ..."
-					mkdir -p $WORKING_PATH/tmp/armmanager
-					sudo cp --remove-destination rasparchitect.sh $WORKING_PATH/tmp/armmanager
-					#Providing daemon
-					sudo cp --remove-destination daemon.tgz $WORKING_PATH/tmp/armmanager
-					if [ $? -ne 0 ]
-					then
-						echo "Couldn't copy script for manipulation inside the image."
-						echo "Please retry or copy it manually"
-						exit 1
-					fi
-				#fi
-				echo "Going to chroot into mounted raspberry pi filesystem ..."
-				echo "Careful ! Now only works for Raspbian"
-				rootasked "chroot"
-				sudo chroot $WORKING_PATH /usr/bin/qemu-arm-static /bin/bash /tmp/armmanager/rasparchitect.sh -a -f=daemon.tgz
-				echo "Finished chroot actions"
+				echo "Please install binfmt-support in order to enable qemu ARM translation"
+				echo "Run 'apt-get install binfmt-support'"
 			fi
 		else
-			echo "Please install binfmt-support in order to enable qemu ARM translation"
-			echo "Run 'apt-get install binfmt-support'"
+			echo "Coudln't find qemu-arm-static executable in image, trying to copy it ..."
+			copy_qemu
+			echo "Retrying to chroot ..."
+			chroot_image
 		fi
 	else
-		echo "Coudln't find qemu-arm-static executable in image, trying to copy it ..."
-		copy_qemu
-		echo "Retrying to chroot ..."
-		chroot_image
+		echo "Given path is not a mounted filesystem, please mount a filesystem before trying to chroot in ..."
+		echo "Exitting ..."
+		exit 1
+	fi
+}
+
+verify_chroot_options(){
+	if [ "$CHROOTOPTIONS" != "" ]
+	then
+		for element in "${CHROOTOPTIONS[@]}"
+		do
+			added=false
+			for option in "${POSSIBLEOPTIONS[@]}"
+			do
+				if [ "$option" == "$element" ]
+				then
+					FINALOPTIONS="$FINALOPTIONS --$option"
+					added=true
+				fi
+			done
+			if [ $added == false ]
+			then
+				echo "Unknown chroot-in option : $element"
+			fi
+		done
+	fi
+
+	if [ $added == false ]
+	then
+		echo "Please verify your chroot-options"
+		exit 1
 	fi
 }
 
@@ -249,8 +289,20 @@ do
 			CHROOT=true
 		shift # past argument=value
 		;;
-		*)
+		-co=*|--chroot-options=*)
+			FILE="${i#*=}"
+			if [ "$FILE" != "$i" ]
+			then
+				IFS=',' read -r -a CHROOTOPTIONS <<< "$FILE"
+				verify_chroot_options
+			fi
+		shift # past argument=value
+		;;
+		-*)
 			echo "Unkown option : $i" # unknown option
+		;;
+		*)
+			echo "Unkown argument : $i" # unknown argument
 		;;
 	esac
 done
@@ -272,3 +324,6 @@ if [ $MOUNTOPT -eq 2 ]
 then
 	unmount_image
 fi
+
+echo "All actions seemed to end correctly, thank you ...!"
+exit 0
--- rasparchitect.sh
+++ rasparchitect.sh
@@ -5,13 +5,20 @@ command_exists(){
     type "$1" &> /dev/null ;
 }
 
+launch_bash(){
+    echo "Launching bash emulator inside image"
+    echo "Just use exit command to quit bash and chroot"
+    bash
+}
+
 #we just modify the PATH ...
 echo "======> Raspbian Preparator"
 echo "Modifying path ..."
 PATH=/bin:$PATH
-ACTIVATED=false
 FILES=""
 EXEC_NAME=mockdaemon
+CHROOT_ONLY=false
+INSTALL_ONLY=false
 
 echo "Moving to /tmp/armmanager"
 cd /tmp/armmanager
@@ -19,13 +26,14 @@ cd /tmp/armmanager
 for i in "$@"
 do
 	case $i in
-		-a|--activated)
-			ACTIVATED=true
+		--chroot-only)
+			CHROOT_ONLY=true
 		;;
+        --install-only)
+            INSTALL_ONLY=true
+        ;;
         -f=*|--files=*)
 			FILES="${i#*=}"
-            echo $FILES
-            ls
 			if [ ! -e "./$FILES" ]
 			then
 				echo "Please give an existing targz archive"
@@ -39,58 +47,75 @@ do
 	esac
 done
 
-echo "======> Installing dependencies ..."
-echo "==> Updating package list"
-#apt-get update
-
-echo "Checking gcc exists ..."
-if ! command_exists gcc;
+if [ $CHROOT_ONLY == true ] && [ $INSTALL_ONLY == true ]
 then
-    echo "Installing gcc ..."
-    #options, to limit output
-    apt-get install -qq -o=Dpkg::Use-Pty=0 gcc
+    echo "So you want to chroot only, but to install only ... What shall I do ?"
+    echo "It's too complicated for me."
+    echo "My answer is 42."
+    exit 42
 fi
 
-if ! command_exists make;
+if [ $CHROOT_ONLY == false ]
 then
-    echo "Installing build-essentials (make)"
-    apt-get install -qq -o=Dpkg::Use-Pty=0 build-essentials
-fi
+    echo "======> Installing dependencies ..."
+    echo "==> Updating package list"
+    #apt-get update
 
-if [ -e "$FILES" ]
-then
-    echo "==> Installing daemon"
-    echo "Decompressing sources ..."
-    ffiles=`basename $FILES`
-    decomp_files=${ffiles%.*}
-    if [ -e $decomp_files ]; then rm -rf $decomp_files; fi
-    tar -xzf $FILES
-    if [ $? -eq 0 ]
+    echo "Checking gcc exists ..."
+    if ! command_exists gcc;
     then
-        echo "Compiling sources ..."
-        make -C ./$decomp_files all
+        echo "Installing gcc ..."
+        #options, to limit output
+        apt-get install -qq -o=Dpkg::Use-Pty=0 gcc
+    fi
+
+    if ! command_exists make;
+    then
+        echo "Installing build-essentials (make)"
+        apt-get install -qq -o=Dpkg::Use-Pty=0 build-essentials
+    fi
+
+    if [ -e "$FILES" ]
+    then
+        echo "==> Installing daemon"
+        echo "Decompressing sources ..."
+        ffiles=`basename $FILES`
+        decomp_files=${ffiles%.*}
+        if [ -e $decomp_files ]; then rm -rf $decomp_files; fi
+        tar -xzf $FILES
         if [ $? -eq 0 ]
         then
-            echo "Copying executable and launching script ..."
-            cp ./$decomp_files/$EXEC_NAME /usr/local/bin/$EXEC_NAME
-            chmod a+x ./$decomp_files/$EXEC_NAME.sh
-            cp ./$decomp_files/$EXEC_NAME.sh /etc/init.d/$EXEC_NAME
-            echo "Adding program to init.d services ..."
-            sudo update-rc.d $EXEC_NAME defaults > /home/pi/armmanager.log
-            echo "Enabling daemon ..."
-            sudo update-rc.d $EXEC_NAME enable >> /home/pi/armmanager.log
+            echo "Compiling sources ..."
+            make -C ./$decomp_files all
+            if [ $? -eq 0 ]
+            then
+                echo "Copying executable and launching script ..."
+                cp ./$decomp_files/$EXEC_NAME /usr/local/bin/$EXEC_NAME
+                chmod a+x ./$decomp_files/$EXEC_NAME.sh
+                cp ./$decomp_files/$EXEC_NAME.sh /etc/init.d/$EXEC_NAME
+                echo "Adding program to init.d services ..."
+                sudo update-rc.d $EXEC_NAME defaults > /home/pi/armmanager.log
+                echo "Enabling daemon ..."
+                sudo update-rc.d $EXEC_NAME enable >> /home/pi/armmanager.log
+            else
+                echo "A problem occured when trying to compile files"
+            fi
         else
-            echo "A problem occured when trying to compile files"
+            echo "Impossible to extract files from targz file"
+            exit 1
         fi
+
+        if [ $INSTALL_ONLY == false ]
+        then
+            launch_bash
+        fi
+
     else
-        echo "Impossible to extract files from targz file"
+        echo "No files provided, nothing to do ..."
+        echo "Exitting ..."
         exit 1
     fi
+else
+    launch_bash
 fi
 
-if [ ACTIVATED ]
-then
-    echo "Launching bash emulator inside image"
-    echo "Just use exit command to quit bash and chroot"
-    bash
-fi
