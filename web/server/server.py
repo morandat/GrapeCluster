3602bfdd324c7fc20852e0c300223503bfd49716
pravez <paul.ravezb@gmail.com>
2017-02-14 12:48:19 +0100

    Pushed a basic idea of how to use flask with our data.

--- web/server/server.py
+++ web/server/server.py
@@ -2,13 +2,28 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/test")
 def template_test():
     return render_template('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
 
-@app.route("/test")
+@app.route("/index")
+@app.route("/")
 def template_debug():
-    return render_template('index.html')
+    raspberries={
+        1:{
+            'name':'Cran',
+            'heat':'70',
+            'os':'Pidora',
+            'state':'On'
+        },
+        2:{
+            'name':'Straw',
+            'heat':'65',
+            'os':'Raspbian',
+            'state':'On'
+        }
+    }
+    return render_template('index.html', raspberries=raspberries)
 
 
 if __name__ == '__main__':
