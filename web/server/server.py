b0a86b93c5297fc37aa1a957720aaa03660fba47
pravez <paul.ravezb@gmail.com>
2017-02-16 22:26:46 +0100

    working on a new structure for the server => maybe do a python package instead of a module

--- web/server/server.py
+++ web/server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
 app = Flask(__name__)
 
 stacksTest = {
@@ -55,6 +55,12 @@ raspsTest = {
     }
 }
 
+#######################################################################################
+#                                                                                     #
+#                                    VIEWS                                            #
+#                                                                                     #
+#######################################################################################
+
 @app.route("/test")
 def template_test():
     return render_template('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
@@ -73,6 +79,53 @@ def api_test():
 def template_details(raspAddr):
     rasp = raspsTest[raspAddr]
     return render_template('rasp.html', rasp=rasp)
+
+#######################################################################################
+#                                                                                     #
+#                                   REST ROUTES                                       #
+#                                                                                     #
+#######################################################################################
+
+########SLAVES#########
+
+@app.route("/slave/", defaults={'id':None}, methods=['GET'])
+@app.route("/slave/<int:id>")
+def slave_details(id):
+    if id is not None:
+        return app.response_class(
+            response=json.dumps(raspsTest[id]),
+            status=200,
+            mimetype='application/json')
+    else:
+        return app.response_class(
+            response=json.dumps(raspsTest),
+            status=200,
+            mimetype='application/json')
+
+@app.route("/conf/", defaults={'id': None}, methods=['GET'])
+@app.route("/conf/<int:id>")
+def slave_conf(id):
+    if id is not None:
+        return app.response_class(
+            response=json.dumps(get_conf(id)),
+            status=200,
+            mimetype='application/json')
+    else:
+        return app.response_class(
+            response=json.dumps(get_conf()),
+            status=200,
+            mimetype='application/json')
+
+conf=[{"ip":"192.168.0.1"}, {"ip":"192.168.0.2"}, {"ip":"192.168.0.3"}]
+
+def get_conf(id=None):
+    if id is not None:
+        return conf[id]
+    else:
+        return conf
+
+#########CLUSTERS#########
+
 
 if __name__ == '__main__':
     app.run(debug=True)
