8b9797ff7b7d2251bf682b9e257c3a0239aecf5b
CyrilBos <bos.cyril33@gmail.com>
2017-03-31 11:37:32 +0200

    created i2cslave.h

---
+++ slave/i2cslave.h
@@ -0,0 +1,12 @@
+//
+// Created by cyrbos on 3/31/17.
+//
+
+#ifndef SLAVE_I2CSLAVE_H
+#define SLAVE_I2CSLAVE_H
+
+#define TX_BUF_SIZE   4000
+
+#define DEFAULT_DEVICE "/dev/i2c_slave"
+
+#endif //SLAVE_I2CSLAVE_H
