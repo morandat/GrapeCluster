e3b9f4148d1289f03d6891521440ecd4ad898a3c
marilafo <marilafo@localhost.localdomain>
2017-04-04 23:59:19 +0000

    test

--- master/Alimentation.py
+++ master/Alimentation.py
@@ -3,7 +3,7 @@ from PiDevice import PiDevice
 
 class Alimentation(PiDevice):
     CLASS_ADDRESS = 0x38  # 42 for slaves
-    def __init__(self, stack_nb=None, mac_add=None, ip_address=None, i2c_add=0x42, pos=None, instruction=0x00, param=None):
+    def __init__(self, stack_nb=None, mac_add=None, ip_address=None, i2c_add=0x38, pos=0, instruction=0x00, param=None):
         super(Alimentation, self).__init__(stack_nb, mac_add, ip_address, i2c_add, pos)
 
     def enable_alimentation(self):
