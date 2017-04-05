34ad0633fda2493dd8ae916e77db3964fd0678ec
bendossantos <bendossantos@enseirb-matmeca.fr>
2017-04-05 19:11:50 +0200

    added shutdown and restart exceptions

--- master/Slave.py
+++ master/Slave.py
@@ -12,32 +12,37 @@ from array import array
 #                6: Slave.error
 #                }
 
+no_return_instructions = {'shutdown', 'restart'}
+
 class Slave(PiDevice):
     #CLASS_ADDRESS = 0x00 # to define for each slave
 
-    def __init__(self, i2c_add, instruction, stack_nb=None, mac_add=None, ip_address=None, pos=None):
-        self.__instr=instruction
-        self.CLASS_ADDRESS=i2c_add
+    def __init__(self, i2c_add, numb_instr, name_instruction, stack_nb=None, mac_add=None, ip_address=None, pos=None):
+        self.__instr = numb_instr
+        self.__name_instruction = name_instruction
+        self.CLASS_ADDRESS = i2c_add
         self.__data = [-1, -1, -1, -1]
         super(Slave, self).__init__(stack_nb, mac_add, ip_address, i2c_add, pos)
-        self.send_instruction()
+        #self.send_instruction()
 
     def send_instruction(self):
         while True:
             try:
                 self.write_byte(self.__instr)
                 break
-            except Exception as e :
+            except Exception as e:
                 print("Write from", self.CLASS_ADDRESS, " failed trying again")
-        for i in range(0, 4):
-            while True:
-                try:
-                    tmp = self.read_byte(self._i2c)
-                    self.__data[i] = tmp
-                    break
-                except Exception as e:
-                    print("Read from", self.CLASS_ADDRESS, "failed, trying again")
-        self.decode_data()
+        if self.__name_instruction not in no_return_instructions:
+            for i in range(0, 4):
+                while True:
+                    try:
+                        tmp = self.read_byte(self._i2c)
+                        self.__data[i] = tmp
+                        break
+                    except Exception as e:
+                        print("Read from", self.CLASS_ADDRESS, "failed, trying again")
+            self.decode_data()
+            return self.decode_data
 
     def decode_data(self):
         print("before decode", self.__data)
@@ -46,29 +51,3 @@ class Slave(PiDevice):
         for i in range(0, 4):
             data_decoded[i] = chr(self.__data[i])
         print("after decode", data_decoded)
-
-    def test_dede(self):
-        self.write_byte(0x00)
-        i = 0
-        e = 0
-        while (i < 4):
-            try :
-                if(e == 0):
-                    tmp = self.read_byte(self._i2c)
-                    print(tmp)
-                    i = i + 1
-                else:
-                    tmp = self.read_byte(self._i2c)
-                    print("err :")
-                    print(tmp)
-                    e = 0
-                #break
-            except Exception as e:
-                e = 1
-                print("plant", e)  
-
-
-
-
-
-
