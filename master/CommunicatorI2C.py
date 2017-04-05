0dfde5bc2da1e68a4cc56ae9faae85de00a9cdf2
bendossantos <bendossantos@enseirb-matmeca.fr>
2017-04-05 12:09:59 +0200

    updated CommunicatorI2C.py calling Slave functions

--- master/CommunicatorI2C.py
+++ master/CommunicatorI2C.py
@@ -3,6 +3,7 @@ from Slave import Slave
 class CommunicatorI2C(Communicator):  # Communicator for I2C
     def __init__(self, i2c_add, instr):
         self.__slv = Slave(i2c_add, instr)
+
     def open_communication(self, i2c_add, instr):
         self.__slv = Slave(i2c_add, instr)
 
