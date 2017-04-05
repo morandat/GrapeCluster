c87b5c63c400dce64a8afacb1d255425cc8ccbe1
bendossantos <bendossantos@enseirb-matmeca.fr>
2017-04-05 12:08:06 +0200

    add little ascii decode line v2

--- master/CommunicatorI2C.py
+++ master/CommunicatorI2C.py
@@ -1,16 +1,11 @@
 from Communicator import Communicator
-
+from Slave import Slave
 class CommunicatorI2C(Communicator):  # Communicator for I2C
-    def __init__(self, port, address):
-        print("init i2c")
-    def open_communication(self):
-        print("open i2c")
+    def __init__(self, i2c_add, instr):
+        self.__slv = Slave(i2c_add, instr)
+    def open_communication(self, i2c_add, instr):
+        self.__slv = Slave(i2c_add, instr)
+
     def send(self, message, address):
-        print("send i2c")
+        self.__slv.send_instruction()
 
-    def receive(self, size):
-        print("receive i2c")
-
-    def close_communication(self):
-        print("close i2c")
-        pass
