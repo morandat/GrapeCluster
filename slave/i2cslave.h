b8ea028e9cc57ca35f4d6ae2f394d0cd13824876
CyrilBos <bos.cyril33@gmail.com>
2017-04-05 20:34:49 +0200

    forgot header

--- slave/i2cslave.h
+++ slave/i2cslave.h
@@ -11,7 +11,7 @@
 
 #define DEFAULT_DEVICE "/dev/i2c_slave"
 
-void i2c_handle(int i2c_fd, char tx_buffer[], int mode, fd_set* wfds);
+void i2c_handle(int i2c_fd, char tx_buffer[], int mode);
 int i2c_init(int* mode, int argc, char* argv[], char **ord);
 
 #endif //SLAVE_I2CSLAVE_H
