d82b89dbb454ecc1a86e80b0619e1c2bde4adb5d
CyrilBos <bos.cyril33@gmail.com>
2017-04-05 20:40:36 +0200

    syntax fix

--- slave/i2cslave.c
+++ slave/i2cslave.c
@@ -18,8 +18,9 @@
 
 char **orders;
 
-void get_order(int order_code, char **order){
+void get_order(int order_code, char **order) {
     *order = orders[order_code];
+}
 
 void action(int call, char **out){
 		
@@ -112,131 +113,35 @@ void i2c_handle(int i2c_fd, char tx_buffer[], int mode) {
 
     for(int i = 0; i < length; i++)
     {
-            switch (mode) {
-            case 1:
-                printf("1: Data received : %c\n", tx_buffer[i]);
-                action(tx_buffer[i], &tx_answer);
-                //write(i2c_fd, endstring, 1);
+        switch (mode) {
+        case 1:
+            printf("1: Data received : %c\n", tx_buffer[i]);
+            action(tx_buffer[i], &tx_answer);
+            //write(i2c_fd, endstring, 1);
 
-                    write(i2c_fd, tx_answer, 4);
+                write(i2c_fd, tx_answer, 4);
 
-                //write(i2c_fd, endstring, 1);
-                break;
-            case 2:
-                printf("2 :Data received : %02x\n ", tx_buffer[i]);
-                action(tx_buffer[i], &tx_answer);
-                //write(i2c_fd, endstring, 1);
+            //write(i2c_fd, endstring, 1);
+            break;
+        case 2:
+            printf("2 :Data received : %02x\n ", tx_buffer[i]);
+            action(tx_buffer[i], &tx_answer);
+            //write(i2c_fd, endstring, 1);
 
-                    write(i2c_fd, tx_answer, 4);
+                write(i2c_fd, tx_answer, 4);
 
-                //write(i2c_fd, endstring, 1);
-                break;
-            default:
-                printf("3 :Data received : %d \n", tx_buffer[i]);
-                action(tx_buffer[i], &tx_answer);
-                //write(i2c_fd, endstring, 1);
-                //write(i2c_fd, tx_answer, 4);
+            //write(i2c_fd, endstring, 1);
+            break;
+        default:
+            printf("3 :Data received : %d \n", tx_buffer[i]);
+            action(tx_buffer[i], &tx_answer);
+            //write(i2c_fd, endstring, 1);
+            //write(i2c_fd, tx_answer, 4);
 
-                    write(i2c_fd, tx_answer, 4);
+                write(i2c_fd, tx_answer, 4);
 
-                //write(i2c_fd, endstring, 1);
-                break;
-            }
+            //write(i2c_fd, endstring, 1);
+            break;
         }
     }
 }
-    //decode_data(com, &is_commande, &nb_opt, tx_buffer);
-
-    //write(i2c_fd, tx_buffer, length);
-
-
-/*
-int main(int argc, char **argv)
-{
-	char tx_buffer[TX_BUF_SIZE];
-	char *tx_answer;
-	int fd;
-	uint8_t data;
-	int length;
-	int i;		
-
-	int opt;
-	int mode = 0; 
-	
-	enum sys_call commande;
-
-	FILE *usage_file = stderr;
-	const char *input = DEFAULT_DEVICE;
-
-	while ((opt = getopt(argc, argv, "hxc")) != -1) {
-		switch (opt) {
-		case 'd':
-			mode = 0; // TODO defines
-			break;
-		case 'c':
-			mode = 1; // TODO defines
-			break;
-		case 'x':
-			mode = 2; // TODO defines
-			break;
-		case 'h':
-			usage_file = stdout;
-		default:
-			fprintf(usage_file, "Usage: %s [-a addr] [-x|-c|-d] [device]\n", argv[0]);
-			fprintf(usage_file, "defaults device: %s addr: %x\n", DEFAULT_DEVICE, 0x42);
-			exit(usage_file == stdout ? EXIT_SUCCESS : EXIT_FAILURE);
-		}
-	}
-
-
-
-	if (optind < argc) {
-		input = argv[optind];
-	}
-
-	if ((fd = open(input, O_RDWR)) == -1) {
-		perror("open i2c device");
-		exit(EXIT_FAILURE);
-	}
-
-
-	char endstring[]={ENDSYMB};
-
-	while (1) {	
-			length = read(fd, tx_buffer, 4);
-			for(i = 0; i < length; i++)
-			{
-				switch (mode) {
-				case 1:
-					printf("1: Data received : %c\n", tx_buffer[i]);
-					tx_answer = action(tx_buffer[i]);
-					write(fd, endstring, 1);
-					write(fd, tx_answer, 4);
-					write(fd, endstring, 1);
-					break;
-				case 2:
-					printf("2 :Data received : %02x\n ", tx_buffer[i]);
-					tx_answer = action(tx_buffer[i]);
-					write(fd, endstring, 1);
-					write(fd, tx_answer, 4);
-					write(fd, endstring, 1);
-					break;
-				default:
-					printf("3 :Data received : %d \n", tx_buffer[i]);
-					tx_answer = action(tx_buffer[i]);
-					write(fd, endstring, 1);
-					write(fd, tx_answer, 4);
-					write(fd, endstring, 1);
-					break;
-				}
-			}
-			tx_buffer[0] = 0; 
-			tx_buffer[1] = 1;
-			tx_buffer[2] = 2; 
-			tx_buffer[3] = 4;
-			write(fd, tx_buffer, 4);
-	}
-
-	close(fd);
-	return 0;
-}*/
