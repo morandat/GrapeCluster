a410930df021d61ff7cde0c127f3042965f011c5
CyrilBos <bos.cyril33@gmail.com>
2017-04-05 19:57:38 +0200

    merged i2c

--- master/grape.py
+++ master/grape.py
@@ -59,6 +59,7 @@ class I2CDevice(object):
     def read_byte(self, register = 0x00):
         return self._bus.read_byte_data(self._address, register)
     def write_byte(self,value, register=0x00):
+        print("beer")
         print(self._address)
         print(register)
         print(value)
