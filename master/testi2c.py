93378bc8b812ba959d14cc1b1e3ddc194eb00791
Marilafo <marilafo@enseirb-matmeca.fr>
2017-04-06 21:35:17 +0000

    jpp

--- master/testi2c.py
+++ master/testi2c.py
@@ -5,8 +5,10 @@ def main_i2c():
     slv = Slave(0x42)
     comm = CommunicatorI2C()
     comm.send_instruction(slv, 0x00, 'test1')
-    print(slv.get_data())
-    comm.send_instruction(slv, 0x01, 'test2')
+    #print(slv.get_data())
+    #comm.send_instruction(slv, 0x00, 'test2')
+    #comm.send_instruction(slv, 0x01, 'test2')
+    #comm.send_instruction(slv, 0x01, 'test2')
     print(slv.get_data())
 
 main_i2c()
